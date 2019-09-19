ab43a3881cf5c2be4e6294ce3331834b261a1775
JT <revirth@gmail.com>
2019-09-18 22:30:02 -0400

    graphql > resolvers > hello

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+require('dotenv').config();
+
+import { GraphQLServer } from 'graphql-yoga';
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = `
+    type Query {
+        hello: String!
+    }`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello'
+    }
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start({ port: PORT }, () => console.log(`Server running on http://localhost:${PORT}`));
