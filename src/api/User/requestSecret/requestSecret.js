81131129e02ab099f7013d36532b9028b4e43f09
JT Lee <revirth@gmail.com>
2019-09-25 19:25:15 -0400

    add requestSecret api

---
+++ src/api/User/requestSecret/requestSecret.js
@@ -0,0 +1,23 @@
+import { generateSecret } from "../../../utils";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    requestSecret: async (_, args) => {
+      const { email } = args;
+
+      const loginSecret = generateSecret();
+
+      console.log("loginSecret :", loginSecret);
+
+      try {
+        await prisma.updateUser({ data: { loginSecret }, where: { email } });
+
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    }
+  }
+};
