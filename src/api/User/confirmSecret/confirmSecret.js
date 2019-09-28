96a10aafbe53ec4531894a3f74f73a8e62ba11b1
JT Lee <revirth@gmail.com>
2019-09-28 13:26:28 -0400

    reset loginSecret value / confirmSecret api

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -8,6 +8,12 @@ export default {
       const user = await prisma.user({ email });
 
       if (user.loginSecret === secret) {
+        // reset loginSecret
+        await prisma.updateUser({
+          where: { id: user.id },
+          data: { loginSecret: "" }
+        });
+
         return generateToken(user.id);
       } else {
         throw Error("Wrong email or secret key");
