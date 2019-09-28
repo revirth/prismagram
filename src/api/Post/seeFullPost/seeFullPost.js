6bd49681b03ba345826b518d1565fdfef67233d9
JT Lee <revirth@gmail.com>
2019-09-28 16:47:17 -0400

    add COMMENT_FRAGMENT to know who leaves a comment

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -1,4 +1,5 @@
 import { prisma } from "../../../../generated/prisma-client";
+import { COMMENT_FRAGMENT } from "../../../fragments";
 
 export default {
   Query: {
@@ -6,7 +7,10 @@ export default {
       const { id } = args;
 
       const post = await prisma.post({ id });
-      const comments = await prisma.post({ id }).comments();
+      const comments = await prisma
+        .post({ id })
+        .comments()
+        .$fragment(COMMENT_FRAGMENT);
       const likeCount = await prisma
         .likesConnection({ where: { post: { id } } })
         .aggregate()
--- src/fragments.js
+++ src/fragments.js
@@ -11,3 +11,14 @@ fragment UserParts on User {
         caption
     }
 }`;
+
+export const COMMENT_FRAGMENT = `
+fragment CommentParts on Comment {
+    id
+    text
+    user {
+        id
+        userName
+    }
+}
+`;
