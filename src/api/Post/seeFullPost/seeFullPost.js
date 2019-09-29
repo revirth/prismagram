a57e0e365a6720a67de9433a9e4540a7b97b96e0
JT Lee <revirth@gmail.com>
2019-09-28 21:21:29 -0400

    update seeFullPost api to get uploaded files

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -7,16 +7,22 @@ export default {
       const { id } = args;
 
       const post = await prisma.post({ id });
+
       const comments = await prisma
         .post({ id })
         .comments()
         .$fragment(COMMENT_FRAGMENT);
+
       const likeCount = await prisma
         .likesConnection({ where: { post: { id } } })
         .aggregate()
         .count();
 
-      return { post, comments, likeCount };
+      const files = await prisma.post({ id }).files();
+
+      const user = await prisma.post({ id }).user();
+
+      return { post, comments, likeCount, files, user };
     }
   }
 };
