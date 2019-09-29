e1bef634c25601a6a2658642dee6c67e5aadd677
JT Lee <revirth@gmail.com>
2019-09-28 20:11:29 -0400

    add computed field - User.isFollowing, User.isMe

--- src/api/User/computedFields.js
+++ src/api/User/computedFields.js
@@ -1,5 +1,21 @@
+import { prisma } from "../../../generated/prisma-client";
+
 export default {
   User: {
-    fullName: parent => `${parent.firstName} ${parent.lastName}`
+    fullName: parent => `${parent.firstName} ${parent.lastName}`,
+    isFollowing: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+
+      return await prisma.$exists.user({
+        AND: [{ id: parentId }, { followers_some: { id: user.id } }]
+      });
+    },
+    isMe: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+
+      return user.id === parentId;
+    }
   }
 };
--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -1,11 +1,12 @@
 import { prisma } from "../../../../generated/prisma-client";
+import { USER_FRAGMENT } from "../../../fragments";
 
 export default {
   Query: {
     seeUser: async (_, args, { request }) => {
       const { id } = args;
 
-      return await prisma.user({ id });
+      return await prisma.user({ id }).$fragment(USER_FRAGMENT);
     }
   }
 };
