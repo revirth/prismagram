a466078672c95e11853b5e58427c6a76ea6672f2
JT Lee <revirth@gmail.com>
2019-10-06 21:51:33 -0400

    Auth Route

--- client/src/Routes/Auth.js
+++ client/src/Routes/Auth.js
@@ -1,1 +1,47 @@
-export default () => "Auth";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  min-height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Box = styled.div`
+  ${props => props.theme.whiteBox}
+  border-radius: 0px;
+  width: 350px;
+`;
+
+const StateChanger = styled(Box)`
+  text-align: center;
+  padding: 20px 0px;
+`;
+
+const Link = styled.span`
+  color: ${props => props.theme.blueColor};
+  cursor: pointer;
+`;
+
+export default () => {
+  const [action, setAction] = useState("login");
+
+  return (
+    <Wrapper>
+      <StateChanger>
+        {action === "login" ? (
+          <>
+            Don't have an account?&nbsp;
+            <Link onClick={() => setAction("signUp")}>Sign Up</Link>
+          </>
+        ) : (
+          <>
+            Have an account?&nbsp;
+            <Link onClick={() => setAction("login")}>Log In</Link>
+          </>
+        )}
+      </StateChanger>
+    </Wrapper>
+  );
+};
