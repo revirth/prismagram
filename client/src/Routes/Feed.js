e2faeb282c0c0cabf1f5a96b4186d382e324e38c
revirth <revirth@gmail.com>
2019-10-17 21:31:07 -0400

    Feed with Loader Component

--- client/src/Routes/Feed.js
+++ client/src/Routes/Feed.js
@@ -1,1 +1,51 @@
-export default () => "Feed";
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
+import Loader from "../Components/Loader";
+import styled from "styled-components";
+import Post from "../Components/Post";
+
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        userName
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          userName
+        }
+      }
+      createdAt
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 80vh;
+`;
+
+export default () => {
+  const { data, loading } = useQuery(FEED_QUERY);
+
+  console.log(data, loading);
+
+  return <Wrapper>{loading && <Loader />}</Wrapper>;
+};
