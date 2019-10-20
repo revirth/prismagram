8000896d2e3d8af5143304f131ec2dd87a826642
revirth <revirth@gmail.com>
2019-10-20 17:54:40 -0400

    add Post Component

---
+++ client/src/Components/Post/PostContainer.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import PostPresenter from "./PostPresenter";
+
+const PostContainer = ({
+  id,
+  user,
+  files,
+  likeCount,
+  isLiked,
+  comments,
+  createdAt,
+  caption,
+  location
+}) => {
+  const [isLikedState, setIsLikedState] = useState(isLiked);
+  const [likeCountState, setLikeCountState] = useState(likeCount);
+  const comment = useState("");
+
+  return (
+    <PostPresenter
+      user={user}
+      files={files}
+      likeCount={likeCountState}
+      isLiked={isLikedState}
+      comments={comments}
+      createdAt={createdAt}
+      newComment={comment}
+      setIsLiked={setIsLikedState}
+      setLikeCount={setLikeCountState}
+      caption={caption}
+      location={location}
+    />
+  );
+};
+
+PostContainer.propTypes = {
+  id: PropTypes.string.isRequired,
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    userName: PropTypes.string.isRequired
+  }).isRequired,
+  files: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  likeCount: PropTypes.number.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      user: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        userName: PropTypes.string.isRequired
+      }).isRequired
+    })
+  ).isRequired,
+  caption: PropTypes.string.isRequired,
+  location: PropTypes.string,
+  createdAt: PropTypes.string.isRequired
+};
+
+export default PostContainer;
