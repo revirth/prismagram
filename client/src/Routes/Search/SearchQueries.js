44608367f70d924e660afee3bb787b708c139fc7
revirth <revirth@gmail.com>
2019-10-24 22:58:47 -0400

    add user.id from the result of searchUser

--- client/src/Routes/Search/SearchQueries.js
+++ client/src/Routes/Search/SearchQueries.js
@@ -3,12 +3,14 @@ import { gql } from "apollo-boost";
 export const SEARCH_QUERY = gql`
   query search($term: String!) {
     searchPost(term: $term) {
+      id
       files {
         url
       }
       likeCount
     }
     searchUser(term: $term) {
+      id
       userName
       avatar
       isFollowing
