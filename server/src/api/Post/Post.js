97163a434349606357e8e1ae4ee225b7f7b9ad97
revirth <revirth@gmail.com>
2019-10-26 15:32:50 -0400

    server - add Post.commentCount

--- server/src/api/Post/Post.js
+++ server/src/api/Post/Post.js
@@ -15,6 +15,11 @@ export default {
         .likesConnection({ where: { post: { id: parent.id } } })
         .aggregate()
         .count(),
+    commentCount: async parent =>
+      await prisma
+        .commentsConnection({ where: { post: { id: parent.id } } })
+        .aggregate()
+        .count(),
     files: async parent => await prisma.post({ id: parent.id }).files(),
     comments: async parent => await prisma.post({ id: parent.id }).comments(),
     user: async parent => await prisma.post({ id: parent.id }).user()
