4797b8397151ab815a07e06614ea73857d682f92
revirth <revirth@gmail.com>
2019-10-30 21:22:56 -0400

    install ApolloClient, InMemoryCache, AsyncStorage

--- app/App.js
+++ app/App.js
@@ -4,15 +4,31 @@ import { AppLoading } from "expo";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
 import { Text, View } from "react-native";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import AsyncStorage from "@react-native-community/async-storage";
+import ApolloClient from "apollo-boost";
+import apolloClientOptions from "./apollo";
+import { ApolloProvider } from "react-apollo-hooks";
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
+
   const preLoad = async () => {
     try {
       await Font.loadAsync({ ...Ionicons.font });
       await Asset.loadAsync([require("./assets/logo.png")]);
 
+      const cache = new InMemoryCache();
+      await persistCache({ cache, storage: AsyncStorage });
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOptions
+      });
+
       setLoaded(true);
+      setClient(client);
     } catch (e) {
       console.error(e);
     }
@@ -22,10 +38,12 @@ export default function App() {
     preLoad();
   }, []);
 
-  return loaded ? (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </ApolloProvider>
   ) : (
     <AppLoading />
   );
