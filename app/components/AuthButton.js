fbb17e755fbff46dba85a44d06dc28119175ff60
revirth <revirth@gmail.com>
2019-11-09 15:52:32 -0500

    AuthButton - add bgColor

--- app/components/AuthButton.js
+++ app/components/AuthButton.js
@@ -7,7 +7,8 @@ import PropTypes from "prop-types";
 const Touchable = styled.TouchableOpacity``;
 
 const Container = styled.View`
-  background-color: ${props => props.theme.blueColor};
+  background-color: ${props =>
+    props.bgColor ? props.bgColor : props.theme.blueColor};
   padding: 10px;
   border-radius: 5px;
   width: ${constants.width / 2};
@@ -19,9 +20,9 @@ const Text = styled.Text`
   font-weight: 600;
 `;
 
-const AuthButton = ({ text, onPress, loading = false }) => (
+const AuthButton = ({ text, onPress, loading = false, bgColor = null }) => (
   <Touchable disabled={loading} onPress={onPress}>
-    <Container>
+    <Container bgColor={bgColor}>
       {loading ? (
         <ActivityIndicator size="large" color="#ffffff" />
       ) : (
