9eca0316e1c87d9f995c68ad8051db5de22ecab2
revirth <revirth@gmail.com>
2019-11-08 23:38:15 -0500

    graphql - createAccount

--- app/screens/Auth/SignUp.js
+++ app/screens/Auth/SignUp.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import AuthButton from "../../components/AuthButton";
+import AuthInput from "../../components/AuthInput";
+import useInput from "../../hooks/useInput";
+import { Alert, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { useMutation } from "react-apollo-hooks";
+import { CREATE_ACCOUNT } from "./AuthQueries";
 
 const View = styled.View`
   flex: 1;
@@ -10,10 +15,86 @@ const View = styled.View`
 
 const Text = styled.Text``;
 
-const SignUp = () => (
-  <View>
-    <AuthButton text="Sign Up" onPress={() => null} />
-  </View>
-);
+const SignUp = ({ navigation }) => {
+  const firstNameInput = useInput("");
+  const lastNameInput = useInput("");
+  const emailInput = useInput(navigation.getParam("email", ""));
+  const userNameInput = useInput("");
+
+  const [loading, setLoading] = useState(false);
+  const [createAccountMutation] = useMutation(CREATE_ACCOUNT, {
+    variables: {
+      userName: userNameInput.value,
+      email: emailInput.value,
+      firstName: firstNameInput.value,
+      lastName: lastNameInput.value
+    }
+  });
+
+  const handleSignUp = async () => {
+    const { value: email } = emailInput;
+    const regxEmail = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    if (!regxEmail.test(email)) return Alert.alert("Email is NOT valid");
+
+    const { value: firstName } = firstNameInput;
+    const { value: lastName } = lastNameInput;
+    const { value: userName } = userNameInput;
+
+    if (firstName === "" || lastName === "")
+      return Alert.alert("Invalid First/Last Name");
+
+    if (userName === "") return Alert.alert("Invalid User Name");
+
+    setLoading(true);
+    try {
+      const { data } = await createAccountMutation();
+      const { createAccount } = data;
+
+      console.log("createAccount", createAccount);
+
+      if (createAccount) {
+        Alert.alert("Account created, login now");
+        navigation.navigate("LogIn", { email });
+      } else {
+        Alert.alert("Can't create account");
+      }
+    } catch (error) {
+      // console.error(error);
+      Alert.alert("The username already exists");
+      // focus/select userNameInput
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View>
+        <AuthInput
+          {...firstNameInput}
+          placeholder="First Name"
+          autoCapitalize="words"
+        />
+        <AuthInput
+          {...lastNameInput}
+          placeholder="Last Name"
+          autoCapitalize="words"
+        />
+        <AuthInput
+          {...emailInput}
+          keyboardType="email-address"
+          placeholder="Email"
+          returnKeyType="send"
+        />
+        <AuthInput
+          {...userNameInput}
+          placeholder="UsreName"
+          returnKeyType="send"
+        />
+        <AuthButton text="SignUp" onPress={handleSignUp} loading={loading} />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
 
 export default SignUp;
