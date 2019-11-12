377061d925501b169c5458134acf8d4f34af0da2
revirth <revirth@gmail.com>
2019-11-11 21:36:26 -0500

    reset NavIcons property

--- app/components/NavIcons.js
+++ app/components/NavIcons.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import PropTypes from "prop-types";
+import styles from "../styles";
 
-const NavIcon = ({ name, color, size = 25 }) => (
-  <Ionicons name={name} color={color} size={size} />
+const NavIcon = ({
+  color = styles.blackColor,
+  focused = true,
+  name,
+  size = 25
+}) => (
+  <Ionicons
+    name={name}
+    color={focused ? color : styles.darkGreyColor}
+    size={size}
+  />
 );
 
 NavIcon.propTypes = {
-  name: PropTypes.string.isRequired,
   color: PropTypes.string,
+  focused: PropTypes.bool,
+  name: PropTypes.string.isRequired,
   size: PropTypes.number
 };
 
