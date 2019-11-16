1a2827da43749d6e570ad2310d699b840c2d9d7f
revirth <revirth@gmail.com>
2019-11-15 21:21:09 -0500

    fix errata

--- app/styles.js
+++ app/styles.js
@@ -3,7 +3,7 @@ export default {
   greyColor: "#F9F9F9",
   darkGreyColor: "#999",
   lightGreyColor: "rgb(230, 230, 230)",
-  readColor: "#ED4956",
+  redColor: "#ED4956",
   blueColor: "#3897f0",
   darkBlueColor: "#003569"
 };
