d458b0a2ceda464a66e56a14eb8610b05eb62fed
revirth <revirth@gmail.com>
2019-11-17 00:08:04 -0500

    change FEED_QUERY to use fragment

--- app/screens/Queries.js
+++ app/screens/Queries.js
@@ -1,31 +1,38 @@
 import { gql } from "apollo-boost";
 
+export const POST_FRAGMENT = gql`
+  fragment PostPart on Post {
+    id
+    location
+    caption
+    user {
+      id
+      avatar
+      userName
+    }
+    files {
+      id
+      url
+    }
+    likeCount
+    isLiked
+    comments {
+      id
+      text
+      user {
+        id
+        userName
+      }
+    }
+    createdAt
+  }
+`;
+
 export const FEED_QUERY = gql`
   {
     seeFeed {
-      id
-      location
-      caption
-      user {
-        id
-        avatar
-        userName
-      }
-      files {
-        id
-        url
-      }
-      likeCount
-      isLiked
-      comments {
-        id
-        text
-        user {
-          id
-          userName
-        }
-      }
-      createdAt
+      ...PostPart
     }
   }
+  ${POST_FRAGMENT}
 `;
