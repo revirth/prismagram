301d701748af4d5781a5780bd61bc867f802f487
revirth <revirth@gmail.com>
2019-11-17 21:01:43 -0500

    add USER_FRAGMENT for graphql

--- app/screens/Queries.js
+++ app/screens/Queries.js
@@ -1,5 +1,7 @@
 import { gql } from "apollo-boost";
 
+//#region POST
+
 export const POST_FRAGMENT = gql`
   fragment PostPart on Post {
     id
@@ -36,3 +38,30 @@ export const FEED_QUERY = gql`
   }
   ${POST_FRAGMENT}
 `;
+
+//#endregion
+
+//#region Profile
+
+export const USER_FRAGMENT = gql`
+  fragment UserPart on User {
+    id
+    userName
+    fullName
+    bio
+    avatar
+    followerCount
+    followingCount
+    isMe
+    isFollowing
+    followerCount
+    followingCount
+    posts {
+      ...PostPart
+    }
+    postCount
+  }
+  ${POST_FRAGMENT}
+`;
+
+//#endregion
